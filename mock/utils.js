1fb337b668c5a373b81b40ee986182ea51c8d105
jiang nan <971959756@qq.com>
2022-02-10 17:59:06 +0800

    xxxx

---
+++ mock/utils.js
@@ -0,0 +1,122 @@
+const os = require('os')
+const fs = require('fs')
+const filepath = './data/wix-data.json'
+
+/**
+ * @param {string} url
+ * @returns {Object}
+ */
+function param2Obj(url) {
+  const search = decodeURIComponent(url.split('?')[1]).replace(/\+/g, ' ')
+  if (!search) {
+    return {}
+  }
+  const obj = {}
+  const searchArr = search.split('&')
+  searchArr.forEach(v => {
+    const index = v.indexOf('=')
+    if (index !== -1) {
+      const name = v.substring(0, index)
+      const val = v.substring(index + 1, v.length)
+      obj[name] = val
+    }
+  })
+  return obj
+}
+
+/**
+ * This is just a simple version of deep copy
+ * Has a lot of edge cases bug
+ * If you want to use a perfect deep copy, use lodash's _.cloneDeep
+ * @param {Object} source
+ * @returns {Object}
+ */
+function deepClone(source) {
+  if (!source && typeof source !== 'object') {
+    throw new Error('error arguments', 'deepClone')
+  }
+  const targetObj = source.constructor === Array ? [] : {}
+  Object.keys(source).forEach(keys => {
+    if (source[keys] && typeof source[keys] === 'object') {
+      targetObj[keys] = deepClone(source[keys])
+    } else {
+      targetObj[keys] = source[keys]
+    }
+  })
+  return targetObj
+}
+
+/**
+ * uuid
+ * @returns {string}
+ */
+function uuid() {
+  var s = []
+  var hexDigits = '0123456789abcdef'
+  for (var i = 0; i < 36; i++) {
+    s[i] = hexDigits.substr(Math.floor(Math.random() * 0x10), 1)
+  }
+  s[14] = '4' // bits 12-15 of the time_hi_and_version field to 0010
+  s[19] = hexDigits.substr((s[19] & 0x3) | 0x8, 1) // bits 6-7 of the clock_seq_hi_and_reserved to 01
+  s[8] = s[13] = s[18] = s[23] = '-'
+  return s.join('')
+}
+
+function getDateTime() {
+  return new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '')
+}
+
+
+function getIPAddress() {
+  let interfaces = os.networkInterfaces()
+  for (var devName in interfaces) {
+    var iface = interfaces[devName]
+    for (var i = 0; i < iface.length; i++) {
+      let alias = iface[i]
+      if (alias.family === 'IPv4' && alias.address !== '127.0.0.1' && !alias.internal) {
+        return alias.address
+      }
+    }
+  }
+}
+
+function getMac() {
+  let interfaces = os.networkInterfaces()
+  for (var devName in interfaces) {
+    var iface = interfaces[devName]
+    for (var i = 0; i < iface.length; i++) {
+      let alias = iface[i]
+      if (alias.family === 'IPv4' && alias.address !== '127.0.0.1' && !alias.internal) {
+        return alias.mac
+      }
+    }
+  }
+}
+
+function getHostname() {
+  return os.hostname()
+}
+
+function getWixData() {
+  let result = []
+  let data = fs.readFileSync(filepath, {'encoding': 'utf8'})
+  if (data) {
+    result = JSON.parse(data)
+  }
+  return result
+}
+function setWixData(data){
+  fs.writeFileSync(filepath, JSON.stringify(data), {'encoding': 'utf8'})
+}
+
+module.exports = {
+  param2Obj,
+  deepClone,
+  uuid,
+  getDateTime,
+  getIPAddress,
+  getHostname,
+  getMac,
+  setWixData,
+  getWixData
+}
