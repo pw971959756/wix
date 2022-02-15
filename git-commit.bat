d2fb814bc619dae0b028daa5ad42ea96bcc7fccc
pw971959756 <971959756@qq.com>
2022-02-15 21:03:01 +0800

    xxx

--- git-commit.bat
+++ git-commit.bat
@@ -1,2 +1,4 @@
+git config --global user.email "zjin053@uottawa.ca"
+git config --global user.name "zjin053@uottawa.ca"
 git commit -m  "%date:~0,4%-%date:~5,2%-%date:~8,2% %time:~0,2%:%time:~3,2%:%time:~6,2%"
 git push -u origin master
