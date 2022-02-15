798ebef3bf733a5d72a9d71cda3fcdd8aef0470e
jiang nan <971959756@qq.com>
2022-02-15 10:16:03 +0800

    xxxx

--- src/views/wix.vue
+++ src/views/wix.vue
@@ -10,14 +10,14 @@
               :header-cell-style="{background:'#eef1f6',color:'#606266'}"
               :default-sort = "{prop: 'lastUpdateDateTime', order: 'descending'}"
               border fit highlight-current-row style="width: 100%;">
-      <el-table-column prop="wixTitle" label="标题" sortable width="180"></el-table-column>
-      <el-table-column prop="wixAuthor" label="作者" sortable width="180"></el-table-column>
-      <el-table-column prop="wixTypesetting" label="排版者" sortable width="180"></el-table-column>
-      <el-table-column prop="wixPublishing" label="出版社" sortable width="180"></el-table-column>
-      <el-table-column prop="wixReleaseDate" label="发表时间" sortable width="180"></el-table-column>
-      <el-table-column prop="wixSummary" label="摘要" sortable width="180"></el-table-column>
-      <el-table-column prop="lastUpdateDateTime" label="最后更新时间" sortable width="180"></el-table-column>
-      <el-table-column label="操作" align="center" class-name="small-padding fixed-width" width="250">
+      <el-table-column prop="wixTitle" label="Title" sortable width="180"></el-table-column>
+      <el-table-column prop="wixAuthor" label="Author" sortable width="180"></el-table-column>
+      <el-table-column prop="wixTypesetting" label="Editor" sortable width="180"></el-table-column>
+      <el-table-column prop="wixPublishing" label="Publisher" sortable width="180"></el-table-column>
+      <el-table-column prop="wixReleaseDate" label="Date" sortable width="180"></el-table-column>
+      <el-table-column prop="wixSummary" label="Abstract" sortable width="180"></el-table-column>
+      <el-table-column prop="lastUpdateDateTime" label="Last Update Date" sortable width="180"></el-table-column>
+      <el-table-column label="Operate" align="center" class-name="small-padding fixed-width" width="250">
         <template slot-scope="{row,$index}">
           <el-button type="warning" @click="handleUpdate(row)" icon="el-icon-edit" ></el-button>
           <el-button v-if="row.status!='deleted'" type="danger" @click="handleDelete(row,$index)" icon="el-icon-delete"></el-button>
@@ -30,58 +30,58 @@
       <el-form ref="dataForm" :rules="rules" :model="temp" label-position="right" label-width="150px">
         <el-row>
           <el-col :span="12">
-            <el-form-item label="标题" prop="wixTitle">
+            <el-form-item label="Title" prop="wixTitle">
               <el-input v-model="temp.wixTitle" ref="roleName" maxlength="50"/>
             </el-form-item>
           </el-col>
           <el-col :span="12">
-            <el-form-item label="连接地址" prop="wixLink">
+            <el-form-item label="Link" prop="wixLink">
               <el-input v-model="temp.wixLink" maxlength="255"/>
             </el-form-item>
           </el-col>
         </el-row>
         <el-row>
           <el-col :span="12">
-            <el-form-item label="作者" prop="wixAuthor">
+            <el-form-item label="Author" prop="wixAuthor">
               <el-input v-model="temp.wixAuthor" ref="roleName" maxlength="50"/>
             </el-form-item>
           </el-col>
           <el-col :span="12">
-            <el-form-item label="排版者" prop="wixTypesetting">
+            <el-form-item label="Editor" prop="wixTypesetting">
               <el-input v-model="temp.wixTypesetting" ref="roleName" maxlength="50"/>
             </el-form-item>
           </el-col>
         </el-row>
         <el-row>
           <el-col :span="12">
-            <el-form-item label="出版社" prop="wixPublishing">
+            <el-form-item label="Publisher" prop="wixPublishing">
               <el-input v-model="temp.wixPublishing" maxlength="255"/>
             </el-form-item>
           </el-col>
           <el-col :span="12">
-            <el-form-item label="发表时间" prop="wixReleaseDate">
-              <el-date-picker v-model="temp.wixReleaseDate" value-format="yyyy-MM-dd" type="date" placeholder="选择日期"></el-date-picker>
+            <el-form-item label="Date" prop="wixReleaseDate">
+              <el-date-picker v-model="temp.wixReleaseDate" value-format="yyyy-MM-dd" type="date" placeholder="select date"></el-date-picker>
             </el-form-item>
           </el-col>
         </el-row>
         <el-row>
           <el-col :span="24">
-            <el-form-item label="摘要" prop="wixSummary">
+            <el-form-item label="Abstract" prop="wixSummary">
               <el-input v-model="temp.wixSummary" maxlength="255"/>
             </el-form-item>
           </el-col>
         </el-row>
         <el-row>
           <el-col :span="24">
-            <el-form-item label="内容" prop="wixContent">
+            <el-form-item label="Content" prop="wixContent">
               <el-input type="textarea" v-model="temp.wixContent"></el-input>
             </el-form-item>
           </el-col>
         </el-row>
       </el-form>
       <div slot="footer" class="dialog-footer">
-        <el-button @click="dialogFormVisible = false">取消</el-button>
-        <el-button :loading="formLoading" type="primary" @click="formSave()">提交</el-button>
+        <el-button @click="dialogFormVisible = false">Cancel</el-button>
+        <el-button :loading="formLoading" type="primary" @click="formSave()">Submit</el-button>
       </div>
     </el-dialog>
   </div>
@@ -110,12 +110,12 @@ export default {
       formLoading: false,
       dialogStatus: '',
       textMap: {
-        update: '编辑数据',
-        create: '新增数据'
+        update: 'Edit',
+        create: 'Add'
       },
       rules: {
-        wixTitle: [{ required: true, message: '标题不能为空', trigger: 'blur' }],
-        wixAuthor: [{ required: true, message: '作者不能为空', trigger: 'blur' }],
+        wixTitle: [{ required: true, message: 'The title can not be blank', trigger: 'blur' }],
+        wixAuthor: [{ required: true, message: 'Author cannot be empty', trigger: 'blur' }],
       },
       temp: {
         id: undefined,
@@ -161,14 +161,14 @@ export default {
     resetTemp() {
       this.temp = {
         id: undefined,
-        wixTitle: undefined,
-        wixAuthor: undefined,
-        wixTypesetting: undefined,
-        wixPublishing: undefined,
-        wixLink: undefined,
-        wixReleaseDate: undefined,
-        wixSummary: undefined,
-        wixContent: undefined
+        wixTitle: "",
+        wixAuthor: "",
+        wixTypesetting: "",
+        wixPublishing: "",
+        wixLink: "",
+        wixReleaseDate: "",
+        wixSummary: "",
+        wixContent: ""
       }
     },
     handleCreate() {
@@ -179,13 +179,13 @@ export default {
     formSave() {
       if (this.temp.id) {
         wixUpdate(this.temp).then(res => {
-          this.$message({message: '操作成功', type: 'success'})
+          this.$message({message: 'Successful operation', type: 'success'})
           this.dialogFormVisible = false
           this.loadListPager()
         })
       } else {
         wixCreate(this.temp).then(res => {
-          this.$message({message: '操作成功', type: 'success'})
+          this.$message({message: 'Successful operation', type: 'success'})
           this.dialogFormVisible = false
           this.loadListPager()
         })
@@ -198,9 +198,9 @@ export default {
       this.dialogFormVisible = true
     },
     handleDelete(row, index) {
-      this.$confirm('是否确定删除?', '确认信息', {distinguishCancelAndClose: true, confirmButtonText: '保存', cancelButtonText: '取消'}).then(() => {
+      this.$confirm('Are you sure you want to delete?', 'Confirm information', {distinguishCancelAndClose: true, confirmButtonText: '保存', cancelButtonText: '取消'}).then(() => {
         wixDelete(row.id).then(res => {
-          this.$message({message: '操作成功', type: 'success'})
+          this.$message({message: 'Successful operation', type: 'success'})
           this.loadListPager()
         })
       })
