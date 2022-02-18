791ebec49f617616f36b387a7f98f920ba9fa6f4
jiang nan <971959756@qq.com>
2022-02-18 16:00:48 +0800

    xxxx

--- git-commit.bat
+++ git-commit.bat
@@ -1,2 +1,2 @@
-git commit -m  "%date:~0,4%-%date:~5,2%-%date:~8,2% %time:~0,2%:%time:~3,2%:%time:~6,2%"
+git commit --allow-empty -m  "%date:~0,4%-%date:~5,2%-%date:~8,2% %time:~0,2%:%time:~3,2%:%time:~6,2%"
 git push -u origin master
